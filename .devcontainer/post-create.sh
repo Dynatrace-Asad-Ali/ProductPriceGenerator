29376bbd9a604e14b5892443a07ae0baba9db47b
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2024-09-06 12:35:01 -0700

    Update post-create.sh
    Reverted back to kind cluster

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install
-#kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
+#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+#sudo install minikube-linux-amd64 /usr/local/bin/minikube
 chmod +x .devcontainer/deployment.sh && .devcontainer/deployment.sh
